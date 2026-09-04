e586c9dba2bc82a5ce46c5bf6089467c645c0cff
amirnourhan-max <amirnourhan@gmail.com>
2026-09-04 13:41:00 +0330

    Remove legacy project clutter

--- backup/Core/CalculationContract.cs
+++
@@ -1,8 +0,0 @@
-namespace GoldBar.Windows.Core;
-
-// Intentionally contains no business formulas yet.
-// The approved UI is locked first; calculation rules will be ported only after
-// they are verified against the user's existing Gold Bar application.
-public interface IGoldCalculationService
-{
-}
--- backup/Core/R4WindowPolicy.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows;
-
-namespace GoldBar.Windows.Core;
-
-public static class R4WindowPolicy
-{
-    public static WindowState StartupState(bool uiSelfTest) =>
-        uiSelfTest ? WindowState.Maximized : WindowState.Normal;
-}
